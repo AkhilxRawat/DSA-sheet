71ddd42660cfc016dc9bf3eaae614e13554980f5
Akhil Singh Rawat <114263685+AkhilxRawat@users.noreply.github.com>
2024-09-10 23:25:11 +0530

    Create loopDetection.java

---
+++ Linked-List/Doubly-Linked-List/loopDetection.java
@@ -0,0 +1,30 @@
+//this is a optimised way using  here tortoise method.
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+       
+        while(fast!=null&&fast.next!=null){
+            
+
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
